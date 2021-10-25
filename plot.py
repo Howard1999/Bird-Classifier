7aafd6f649785f192b4c2cb55b27963f11fb2d50
SuYiHao <suyihao1999@gmail.com>
2021-10-25 23:39:03 +0800

    save figure

--- plot.py
+++ plot.py
@@ -8,3 +8,12 @@ def show_hist(hist_list, title, legends):
     plt.xlabel('Epoch')
     plt.legend(legends, loc='center right')
     plt.show()
+    
+def save_hist(hist_list, title, legends):
+    plt.title(title)
+    for i in range(len(hist_list)):
+        plt.plot(hist_list[i])
+    plt.xlabel('Epoch')
+    plt.legend(legends, loc='center right')
+    plt.savefig(title)
+    plt.close()
